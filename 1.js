0699dbb15600347114669642c49b0542fdfa604f
ShamHason <shamhasonn@gmail.com>
2024-11-29 16:00:40 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+`use strict`
+//sham hason
+const num = 15;
+
+let isDivdableBy2 = Number(num%2 === 0);
+let isDivdableBy3 = Number(num%3 === 0);
+let isDivdableBy5 = Number(num%5 === 0);
+
+console.log(isDivdableBy2+isDivdableBy3+isDivdableBy5);
+
+
